c2076630961c8ee162a390a58712e2c4b781e1ef
Cristian-Ioan Vasile <cvasile@mit.edu>
2018-05-30 22:20:06 -0400

    Updated and added logging information that conforms with the postprocessing format. Performed some clean up.

--- src/planning/localrrt.py
+++ src/planning/localrrt.py
@@ -56,17 +56,17 @@ def monitor(start, buchi, start_prop, stop_prop=None):
     start_prop -- proposition of the start configuration
     stop_prop -- proposition of the stop configuration
             if not given, it will force the computation of the next Buchi states
-    
+
     returns stop  -- is the vector of Buchi states corresponding to the stop
              configuration from the local transition system
-    
+
     Note:
     Assumes that the line segment [start; stop] crosses the boundary of a single
     region once.
     '''
     if start_prop == stop_prop:
         return set(start)
-    
+
     stop = set()
     for buchi_state in start:
         stop |= set(buchi.next_states(buchi_state, stop_prop))
@@ -85,7 +85,7 @@ class LocalPlanner(object):
     '''Local planner generating a random tree within the sensing area of the
     robot.
     '''
-    
+
     def __init__(self, pa, ts, robot, local_spec, eta=0.1):
         '''Constructor'''
         self.pa = pa
@@ -93,25 +93,43 @@ class LocalPlanner(object):
         self.robot = robot
         self.priority = local_spec
         self.lts = None
-        
+
         self.PointCls = Point2D
-        
+
         self.local_plan = None
         self.tracking_req = None
         self.requests = []
         self.obstacles = []
-        
+
         self.trajectory = [self.robot.currentConf]
-        self.buchi_states = [set([s for _, s in self.pa.init])]
-        self.potential = [get_actual_potential(self.robot.initConf,
-                                               self.buchi_states[0], self.pa)]
+        self.buchi_states = None
+        self.potential = None
+        if self.pa is not None:
+            self.buchi_states = set([s for _, s in self.pa.init])
+            self.potential = get_actual_potential(self.robot.initConf,
+                                                  self.buchi_states, self.pa)
         self.global_target_state = self.robot.initConf
-        
+
         # set max steer step size eta and sensing radius
         self.eta = eta
 
-        self.durations = []
-        self.sizes = []
+        logging.info('"initialize local planner": True')
+        self.log()
+
+    def log(self, gen_tree=False):
+        '''Saves information about the current planner status to log.'''
+        logging.info('"Buchi states": %s', self.buchi_states)
+        logging.info('"potential": %f', self.potential)
+        logging.info('"tree size": %d',
+                     self.lts.g.number_of_nodes() if gen_tree else 0)
+        logging.info('"tree": %s', self.lts.g.edges() if gen_tree else [])
+        if self.local_plan is None:
+            logging.info('"new configuration": %s', self.robot.currentConf)
+            logging.info('"local plan": %s', [])
+        else:
+            logging.info('"new configuration": %s', self.local_plan[0])
+            logging.info('"local plan": %s', self.local_plan)
+        logging.info('"requests": %s', self.robot.sensor.requests)
 
     def execute(self, requests, obstacles):
         '''Plan locally.'''
@@ -119,37 +137,21 @@ class LocalPlanner(object):
         self.requests = requests
         self.obstacles = obstacles
         # update local plan
-        local_planning_timer = Timer('Generate Local Plan')
-        with local_planning_timer:
-            nr_nodes = -1
-            
+        with Timer(op_name='local planning', template='"%s runtime": %f'):
+            gen_tree=False
             # if global state => switch to next global state
             current_state = self.robot.currentConf
             if current_state in self.ts.g:
                 final_state = self.min_potential_global_state(current_state)
                 self.global_target_state = final_state
-            
+
             if not self.check_local_plan():
-                logging.debug('Local check: False')
-                self.local_plan, nr_nodes = self.generate_local_plan()
-            else:
-                logging.debug('Local check: True')
-        # update meta data
-        self.durations.append(local_planning_timer.duration)
-        self.sizes.append(nr_nodes)
-        
-        # TODO: this is badly hacked; should implement it with a post-processing
-        # function
-        if nr_nodes > 0:
-            import os # TODO: very bad... should be ashamed of it
-            outputdir = os.path.abspath('../data_ijrr/example2')
-            fname = 'lts_{:0>4d}.yaml'.format(len(self.durations)-1)
-            with open(os.path.join(outputdir, fname), 'w') as ltsf:
-                print>>ltsf, self.lts.init
-                print>>ltsf, self.lts.g.nodes(data=True)
-                print>>ltsf, self.lts.g.edges(data=True)
-        
+                self.local_plan, gen_tree = self.generate_local_plan()
+
         self.update()
+
+        self.log(gen_tree)
+
         # return next point
         return self.local_plan.pop(0)
 
@@ -161,15 +163,15 @@ class LocalPlanner(object):
         target = self.tracking_req.name
         for conf in self.local_plan:
             if target in self.robot.getSymbols(conf, local=True):
-                logging.debug("local_plan_hit: True!")
+#                 logging.debug("local_plan_hit: True!")
                 return True
-        logging.debug("local_plan_hit: False!")
+#         logging.debug("local_plan_hit: False!")
         return False
 
     def check_local_plan(self):
         '''Checks if the local plan is still satisfying with respect to the
         local specification.
-        
+
         NOTE: Assumes the path is satisfying w.r.t. the global specification.
         It does not check collision with global regions.
         NOTE: Sets/Resets the local target request
@@ -197,7 +199,7 @@ class LocalPlanner(object):
         # 2. check that path is collision free
         assert self.local_plan[-1] in self.ts.g # last state is global
         r = self.robot.collision_free(self.local_plan, self.obstacles)
-        logging.debug('Collision check with local obstacles: %s', r)
+#         logging.debug('Collision check with local obstacles: %s', r)
         return r
 
     def update(self):
@@ -206,21 +208,20 @@ class LocalPlanner(object):
         new_conf = self.local_plan[0]
         prop = self.robot.getSymbols(new_conf)
         prev_prop = self.robot.getSymbols(self.robot.currentConf)
-        B = monitor(self.buchi_states[-1], self.pa.buchi, prev_prop, prop)
+        B = monitor(self.buchi_states, self.pa.buchi, prev_prop, prop)
         self.trajectory.append(new_conf)
-        self.buchi_states.append(B)
-        
+        self.buchi_states = B
+
         if new_conf in self.ts.g:
-            self.potential.append(get_actual_potential(new_conf, B, self.pa))
+            self.potential = get_actual_potential(new_conf, B, self.pa)
 
     def min_potential_global_state(self, state):
         '''Computes the minimum potential global state to connect to.'''
-        pa_states = [(state, buchi_state)
-                                    for buchi_state in self.buchi_states[-1]]
+        pa_states = [(state, buchi_state) for buchi_state in self.buchi_states]
         _, pa_next_states = zip(*self.pa.g.out_edges_iter(pa_states))
         opt_pa_next_state = min(pa_next_states,
                            key=lambda x: self.pa.g.node[x]['potential'])
-        if (self.potential[-1] == 0 and
+        if (self.potential == 0 and
             self.pa.g.node[opt_pa_next_state]['potential'] == 0):
             pa_next_states.remove(opt_pa_next_state)
             opt_pa_next_state = min(pa_next_states,
@@ -238,12 +239,12 @@ class LocalPlanner(object):
         # get current state
         if not current_state:
             current_state = self.robot.currentConf
-        
+
         final_state = self.global_target_state
         # check if it can be connected
         if not self.robot.isSimpleSegment(current_state, final_state):
             return []
-        
+
         # generate straight path to the node
         current_state = np.array(current_state.coords)
         final_state = np.array(final_state.coords)
@@ -262,16 +263,16 @@ class LocalPlanner(object):
             return False
         if state in self.ts.g: # local nodes may not be in the global ts
             return False
-        
+
         # test if the target was hit
         target = self.tracking_req
         if target:
             if not self.lts.g.node[state]['hit']:
                 return False
-        
+
         r = self.robot.collision_free_segment(state, self.global_target_state,
                                               self.obstacles)
-        logging.debug('Test:collision_checking: %s', r)
+
         # test if the node can be connected to the global ts
         return (self.robot.isSimpleSegment(state, self.global_target_state)
                 and r)
@@ -283,7 +284,7 @@ class LocalPlanner(object):
         NOTE: exclude current position as next target position
         NOTE: free movement if there are no local requests
         '''
-        
+
         local_requests, local_obstacles = self.requests, self.obstacles
         # 0. no local requests => move towards global state of minimum potential
         if not local_requests:
@@ -297,19 +298,19 @@ class LocalPlanner(object):
             if collision_free:
                 local_plan = self.free_movement()
                 if local_plan:
-                    return local_plan, -1
-        
+                    return local_plan, False
+
         target = self.tracking_req
-        
+
         # 1. initialize local ts
         self.lts = Ts()
         # add current state to local ts
         global_prop = self.robot.getSymbols(self.robot.currentConf)
         local_prop = self.robot.getSymbols(self.robot.currentConf, local=True)
         self.lts.g.add_node(self.robot.currentConf, global_prop=global_prop,
-                       buchi_states=self.buchi_states[-1],
+                       buchi_states=self.buchi_states,
                        hit=(target.name in local_prop) if target else False)
-        
+
         dest_state = self.robot.currentConf
         # test if solution was found
         while not self.test(dest_state):
@@ -322,13 +323,13 @@ class LocalPlanner(object):
             # 6. label new sample
             dest_global_prop = self.robot.getSymbols(dest_state)
             dest_local_prop = self.robot.getSymbols(dest_state, local=True)
-            
+
             if self.robot.isSimpleSegment(source_state, dest_state):
                 # 7. compute Buchi states for new sample
                 source_data = self.lts.g.node[source_state]
                 B = monitor(source_data['buchi_states'], self.pa.buchi,
                             source_data['global_prop'], dest_global_prop)
-                
+
                 if B:
                     if self.robot.collision_free_segment(source_state,
                                                   dest_state, local_obstacles):
@@ -337,48 +338,19 @@ class LocalPlanner(object):
                         if target:
                             hit = ((target.name in dest_local_prop)
                                                         or source_data['hit'])
-                        
+
                         self.lts.g.add_node(dest_state,
                                             global_prop=dest_global_prop,
                                             buchi_states=B, hit=hit)
                         self.lts.g.add_edge(source_state, dest_state)
-            
+
             if dest_state not in self.lts.g:
                 dest_state = None
-            
-#             if logging.getLogger().isEnabledFor(logging.DEBUG):
-#                 r = self.test(dest_state)
-#                 # uncomment for debugging
-#                 if dest_state and r:
-#                     logging.debug('[generate_local_plan] local tree')
-#                     logging.debug('[generate_local_plan] dest_state: %s',
-#                                   dest_state)
-#                     logging.debug('[generate_local_plan] hit: %s', hit)
-#                     logging.debug('[generate_local_plan] dest_local_prop: %s',
-#                                   dest_local_prop)
-#                     if target:
-#                         logging.debug('[generate_local_plan] target.name: %s',
-#                                       target.name)
-#                     logging.debug('[generate_local_plan] test: %s', r)
-# #                     self.sim.display(expanded=True, localinfo=('tree',))
-#                 else:
-#                     logging.debug('[generate_local_plan] rejected')
-        
+
+            # TODO: save relevant data here
+
         # 11. return local plan
         plan_to_leaf = nx.shortest_path(self.lts.g, self.robot.currentConf,
                                         dest_state)[1:]
-        
-#         if logging.getLogger().isEnabledFor(logging.DEBUG):
-#             ll = self.local_plan
-#             
-#             logging.debug('plan to leaf: %s', plan_to_leaf)
-#             logging.debug('dest state: %s', dest_state)
-#             
-#             logging.debug('free movement: %s', self.free_movement(dest_state))
-#             
-#             self.local_plan = plan_to_leaf + self.free_movement(dest_state)
-#             self.sim.display(expanded=True, localinfo=('plan', 'trajectory'))
-#             self.local_plan = ll
-        
-        return (plan_to_leaf + self.free_movement(dest_state),
-                self.lts.g.number_of_nodes())
+
+        return (plan_to_leaf + self.free_movement(dest_state), True)
